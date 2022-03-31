0a6275e2c4720509dd64770698faab00713e5064
rizalyoga <ach.rizal.yogaswara@gmail.com>
2022-03-31 22:09:09 +0700

    setting API

---
+++ src/API/Api.ts
@@ -0,0 +1,28 @@
+// https://opentdb.com/api.php?amount=20&type=multiple
+
+import { shuffleArray } from "../utils";
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+export type QuestionsState = Question & { answers: string[] };
+
+export enum Difficulty {
+  EASY = "easy",
+  MEDIUM = "medium",
+  HARD = "hard",
+}
+
+export const fetchQuizApi = async (amount: number, difficulty: Difficulty) => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  const data = await (await fetch(endpoint)).json();
+  return data.results.map((question: Question) => ({
+    ...question,
+    answers: shuffleArray([...question.incorrect_answers, question.correct_answer]),
+  }));
+};
--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,16 +1,30 @@
 import React, { useState } from "react";
+import { fetchQuizApi } from "../API/Api";
 /* -------------------------------- component ------------------------------- */
 import QuestionCard from "../components/QuestionCard";
+
+/* ---------------------------------- Types --------------------------------- */
+
+import { QuestionsState, Difficulty } from "../API/Api";
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+};
 
 const TOTAL_QUESTIONS = 20;
 
 const QuisPage = () => {
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionsState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswer, setUsetAnswer] = useState([]);
+  const [userAnswer, setUsetAnswer] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizApi(TOTAL_QUESTIONS, Difficulty.EASY));
 
   const startTrivia = async () => {};
 
@@ -26,7 +40,7 @@ const QuisPage = () => {
       </button>
       <p className="score">score :</p>
       <p>Loading Question</p>
-      <QuestionCard questionsNumber={number + 1} totalQuestions={TOTAL_QUESTIONS} question={questions[number].question} answer={questions[number].answer} userAnswer={userAnswer ? userAnswer[number] : undefined} callback={checkAnswer} />
+      {/* <QuestionCard questionsNumber={number + 1} totalQuestions={TOTAL_QUESTIONS} question={questions[number].question} answer={questions[number].answer} userAnswer={userAnswer ? userAnswer[number] : undefined} callback={checkAnswer} /> */}
       <button className="next" onClick={nextQuestions}>
         Next Question
       </button>
